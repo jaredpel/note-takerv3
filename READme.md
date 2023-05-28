51cfd96e144fad98fa5f1868c51c364d8a2c46e7
Jared Pelaez <jaredpelaez@Jareds-iMac.local>
2023-05-27 18:46:27 -0700

    init repo

---
+++ READme.md
@@ -0,0 +1,8 @@
+ ## Description
+   this note taker has an operating server but does not keep notes, tried multiple times to save but only types. re worked and structured code in order and nothing seems to wokr and the css style has gotten messed up once i got the server working, checked to see what was wrong from the original code i downloaded could not find the issue after multiple VSCODE files and the notes still does not save or add more notes(will work on this futher)
+
+ ## Table of content
+
+    ##Usage
+    open terminal in VScode after installing run code 'npm start' or 'node server.js' it will have server up and active but wont be able to save any notes
+    
---
+++ server.js
@@ -0,0 +1,55 @@
+const fs = require('fs');
+const express = require("express");
+const path = require("path");
+const app = express();
+const PORT = process.env.PORT || 3001;
+const { notes } = require("./db/db");
+
+//http://localhost:3001
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+const generateUniqueId = require('generate-unique-id');
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+    fs.writeFileSync(
+      path.join(__dirname, './db/db.json'),
+      JSON.stringify({ notes: notesArray }, null, 2)
+    );
+    return note;
+  };  
+
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, './public/notes.html'));
+});
+
+app.get('/api/notes', (req, res) => {
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  req.body.id = generateUniqueId();
+  const note = createNewNote(req.body, notes);
+  res.json(note);
+});    
+
+app.delete('/api/notes/:id', (req, res) => {
+  const { id } = req.params;
+
+  const delNote = notes.findIndex(note => note.id ==id);
+
+  notes.splice(delNote, 1);
+  return res.send();
+});
+app.listen(PORT, () => {
+    console.log(`App listening on PORT ${PORT}`);
+});
